3e1b01e8529e04f438f186f0688d3894d8d8878b
Davide Cesare <davidereaper@gmail.com>
2020-11-02 16:52:15 +0100

    View list

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        @foreach ($comics as $comic)
+            <div class="d_flex_cont">
+                <img src="{{$comic->image}}" alt="{{$comic->title}}">
+                <h3>{{$comic->title}}</h3>
+                <p>{{$comic->original_title}}</p>
+                <p>{{$comic->author}}</p>
+                <a href="">Show more</a>
+            </div>
+        @endforeach
+    </div>
+</body>
+</html>
